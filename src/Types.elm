de26d506a06eda1db9539f8a77cf3bb178ae7586
Javin <bennyfrost2@gmail.com>
2026-01-16 20:11:45 -0800

    made view board more efficient didnt have to spend 24 hours, created main.elm to run tests

--- src/Types.elm
+++ src/Types.elm
@@ -1,23 +1,3 @@
 module Types exposing (..)
 
-
-{-| Represents which player owns a piece
--}
-type Player
-    = Player1
-    | Player2
-
-
-{-| Represents a position on the board (0-23)
-    The board has 24 positions arranged in 3 concentric squares
--}
-type alias Position =
-    Int
-
-
-{-| Represents a piece on the board
--}
-type alias Piece =
-    { player : Player
-    , position : Position
-    }
+-- Add your type definitions here
