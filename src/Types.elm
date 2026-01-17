37165f8cfc269a96cad8f17cbdc9c934375adc4e
ggarson <graygarson@gmail.com>
2026-01-16 20:10:05 -0800

    added types

--- src/Types.elm
+++ src/Types.elm
@@ -1,3 +1,14 @@
 module Types exposing (..)
 
 -- Add your type definitions here
+type Player
+  = White
+  | Black
+
+type alias Board =
+  List (Maybe Piece)
+
+type alias Piece = 
+  { player : Player
+  , position : Int
+  }
