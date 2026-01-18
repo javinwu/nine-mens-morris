9b466f9e9bbc383a0bf0c58a7181d65264f6f851
ggarson <graygarson@gmail.com>
2026-01-17 23:04:41 -0800

    javin made too many ai comments

--- src/Board.elm
+++ src/Board.elm
@@ -27,7 +27,6 @@ import List.Extra exposing (elemIndex)
 import Types exposing (Piece)
 import Types exposing (Color)
 
-
 -- BOARD DATA STRUCTURES
 
 -- Initial empty board with 24 positions
@@ -35,21 +34,14 @@ import Types exposing (Color)
 pieces : List (Maybe Piece)
 pieces = List.repeat 24 Nothing
 
-
--- Adjacency list for all 24 positions
 -- Each position maps to a list of positions it connects to
--- Example: Position 0 connects to positions 1 and 7
--- This is used to validate moves in the Movement phase (can only move to adjacent positions)
 adjacencies : List (List Int)
 adjacencies
   = [[1,7], [0,2,9], [1,3], [2,4,11], [5,3], [4,6,13], [5,7], [0,6,15]       -- Outer square (0-7)
   , [9,15], [1,8,10,17], [9,11], [3,10,12,19], [11,13], [5,12,14,21], [13,15], [7,8,14,23]  -- Middle square (8-15)
   , [17,23], [9,16,18], [17,10], [11,18,20], [19,21], [13,20,22], [21,23], [15,16,22]]      -- Inner square (16-23)
 
-
 -- All possible mill combinations (3 in a row)
--- A mill is formed when a player has pieces on all 3 positions in any of these combinations
--- 16 total mills: 12 along edges (3 per square) + 4 connecting lines between squares
 possibleMills : List (List Int)
 possibleMills
   = [[0,1,2], [2,3,4], [4,5,6], [6,7,0]           -- Outer square (4 mills)
@@ -57,24 +49,19 @@ possibleMills
   , [16,17,18], [18,19,20], [20,21,22], [22,23,16]  -- Inner square (4 mills)
   , [1,9,17], [3,11,19], [5,13,21], [7,15,23]]    -- Connecting lines (4 mills)
 
+
 -- BOARD QUERY FUNCTIONS
 -- These functions help query and check the state of the board
 
-
 -- Get all adjacent positions for a given piece
--- Used in Movement phase to validate that a move is to an adjacent position
--- Example: If piece is at position 0, returns [1, 7]
 getAdjacencies : Piece -> List Int
 getAdjacencies piece =
   List.drop piece.position adjacencies  -- Skip to the piece's position in the adjacency list
     |> List.head                        -- Get the adjacency list for that position
-    |> Maybe.withDefault []             -- Return empty list if position invalid
+    |> Maybe.withDefault []             -- Return position, return empty list if position invalid
 
 
 -- Get the color of the piece at a specific position
--- Returns Nothing if position is empty or invalid
--- Returns Just color if there's a piece at that position
--- Example: getPieceAt 5 board -> Just White (if white piece at position 5)
 getPieceAt : Int -> List (Maybe Piece) -> Maybe Color
 getPieceAt position board =
   board
@@ -83,26 +70,22 @@ getPieceAt position board =
     |> Maybe.andThen identity      -- Flatten Maybe (Maybe Piece) to Maybe Piece
     |> Maybe.map .color            -- Extract color if piece exists
 
-
--- Check if a position on the board is empty
--- Returns True if position has no piece, False if occupied
--- Used to validate piece placement and movement
+-- stupid function for stupid stupid boy
 isPositionEmpty : Int -> List (Maybe Piece) -> Bool
 isPositionEmpty position board =
-  getPieceAt position board == Nothing  -- Empty if no color found
+  getPieceAt position board == Nothing  -- entire function is literally just this line
 
 
 -- Check if a specific piece is part of a mill on the given board
 -- Returns True if the given piece is part of a completed mill (3 in a row)
 -- This checks if all 3 positions in the mill containing this piece have the same color
--- Used to determine if a player just formed a mill with their move
 isMill : Piece -> List (Maybe Piece) -> Bool
 isMill piece board =
-  case List.Extra.elemIndex piece.position (List.concat possibleMills) of
-    Just a -> possibleMills
-      |> List.drop (floor ((toFloat a) / 4))
-      |> List.head
-      |> Maybe.withDefault []
-      |> List.map (\pos -> getPieceAt pos board)
-      |> List.all (\color -> color == Just piece.color)
-    Nothing -> False
+  case List.Extra.elemIndex piece.position (List.concat possibleMills) of  -- find the mill containing this piece (have to use concatenated list)
+    Just a -> possibleMills                                                -- Get the mill containing this piece
+      |> List.drop (floor ((toFloat a) / 4))                               -- use the index to find the correct mill since used concatenated list before
+      |> List.head                                                         -- get the mill positions
+      |> Maybe.withDefault []                                              -- make it a maybe
+      |> List.map (\pos -> getPieceAt pos board)                           -- get the color of each position
+      |> List.all (\color -> color == Just piece.color)                    -- check if all colors match
+    Nothing -> False                                                       -- make elm happy and satisfy the null case
